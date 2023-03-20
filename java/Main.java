3c4699334976d5be43943075a5e690110dc22eaf
jaredsbt <jaredsbt@gmail.com>
2023-03-20 15:51:22 -0700

    Play / Exit commnads

--- java/Main.java
+++ java/Main.java
@@ -1,41 +1,31 @@
 import java.util.Scanner;
-
-
-
-//Dog dog = new Dog("Brady", 3, "Terrier", "Brown");
-
-public class Main {
+class main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Welcome to Murder Mystery!\n\n There has been multiple deaths reported in the Town of Saint Everking's this past week.\n Your Job is to discover the cause of these deaths.\n Can you solve this MYSTERY?\n\n");
+        System.out.println("Yes, I'm ready (type: play)\n");
 
         boolean isOther = true;
         while (isOther) {
-            System.out.println("Type play to play");
-            System.out.println("Type exit to exit");
-
             try (Scanner confirmation = new Scanner(System.in)) {
-                System.out.println("enter input");
-
                 String userInput = confirmation.nextLine();
                 System.out.println("User confirmation is: " + userInput);
 
-                int characterDifference = "exit".compareToIgnoreCase(userInput);
-                if (characterDifference == 0) { // they're the same strings
-                    System.exit(0);
-                }
-                int characterDifference2 = "play".compareToIgnoreCase(userInput);
-                if (characterDifference2 == 0) {
-                    break;
-                }
+                if (userInput.compareToIgnoreCase("exit") == 0 || userInput.compareToIgnoreCase("no") == 0) { System.exit(0); }
+
+                else if (userInput.compareToIgnoreCase("play") == 0) { break; }
+
+                else { System.out.println("That is a verb I do not recognize"); }
             }
-
         }
 
-        DaySuspects farmer = new DaySuspects("John", "Farmer");             // definition of the object Farmer
-         farmer.interact(); 
-         System.out.println(farmer.givenName);
-        }
+        System.out.println("\nWelcome to Saint Everking's, \n\nThe mayor enforced a 10pm curfew in response to the recent murder after sun set. Police and investigators have not been able to find the criminal.\n\n You, the country's infamous detective, are the Town's only hope. What would you like to be referred as?\n");
+    }
 }
+
+        // DaySuspects farmer = new DaySuspects("John", "Farmer");             // definition of the object Farmer
+        //  farmer.interact(); 
+        //  System.out.println(farmer.givenName);
+        // }
 
 // To create java executable, run javac main.java
 // To run exe. , type java Main
