1914d78e7fe351b3f87c1acdc60c2bf15b1f1234
jaredsbt <jaredsbt@gmail.com>
2023-03-20 17:06:52 -0700

    Fixed name and name confirmation interface

--- java/Adventure.java
+++ java/Adventure.java
@@ -25,11 +25,12 @@ public class Adventure {
         }
 
         System.out.println("\nWelcome to Saint Everking's, \n\nThe mayor enforced a 10pm curfew in response to the recent murder after sun set. Police and investigators have not been able to find the criminal.\n\n You, the country's infamous detective, are the Town's only hope.\n");
-        System.out.println("\nWhat would you like to be called?");
+        
 
         boolean invalidName = true;
         
         while (invalidName) {
+            System.out.println("\nWhat would you like to be called?");
             String nameInput = input.nextLine();
 
             if ( nameInput.length() > 20) { System.out.println("Character too long"); }
@@ -41,18 +42,16 @@ public class Adventure {
                 boolean Name = true;
                 while (Name) {
                     System.out.println(" Is this the name you want to keep: " + nameInput + " ?\n(Yes or no)");
-                    try ( Scanner confirmation = new Scanner(System.in)) {
-                        String confirmationInput = confirmation.nextLine();
+                        String correctName = input.nextLine();
 
-                        if (confirmationInput.compareToIgnoreCase("yes") == 0 ) { break; }
-                        else if ( confirmationInput.compareToIgnoreCase("no") == 0 ) { Name = false; }
+                        if ( correctName.compareToIgnoreCase("yes") == 0 ) { invalidName = false; break; }
+                        else if ( correctName.compareToIgnoreCase("no") == 0 ) { Name = false; }
                         else { System.out.println("That is a very I do not recognize"); }
-                    }
                 }  
             }
         }
 
-        System.out.println("Hello");
+        System.out.println("ur gay");
     }
 }
 
